2df83e72cab7feb6cf732c0ec5b70196eb1bef8e
Noor Jamal Hajbi <noor.hajbi@gmail.com>
2021-03-22 15:42:00 +0200

    add lab 6

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
+
 //User's Feedback:
         
         /*Prompt function(): returns what user input in a box. otherwise null.
         When? if we want the user to input a value at first.
         */
-        /*
-        var x= window.prompt("What is Your name?");
-        while (x == '') {
-           //if (x == '')
-                x= window.prompt("please enter your name");        
-        }
-        
-        var y= window.prompt("What Backgound color you want to choose\n1.White\n2.black\n3.purple\n4.none");
-        /*confirm() function: if the user clicked (OK) returns true, otherwise false.
+       /*confirm() function: if the user clicked (OK) returns true, otherwise false.
         When? if we want to take the user's confirmation about something;
         For Example: as E-commerce websites,
          if you want to delete a product from a cart "Are you Sure?" Message will appear
           and without user's confirmation the product will not be removed.
         */
-       /*
+
+        var x= window.prompt("What is Your name?");
+        while (x == '') {
+           //if (x == '')
+                x= window.prompt("please enter your name");        
+        }
+        /*
+        var y= window.prompt("What Backgound color you want to choose\n1.White\n2.black\n3.purple\n4.none");
+        
         while (y) {
             if (y==1)
                 document.body.style.backgroundColor = "white";
@@ -37,26 +38,46 @@
         }
         y = !y;
         }
-       
-        var imgIndex = new Array(); 
-        //insert the URL of images in array  
-        imgIndex[0] = "https://64.media.tumblr.com/0a423f1560b6379d350f3c997e73f57e/tumblr_p4vqvrVfca1x72yt1o1_500.png";  
-        imgIndex[1] = "https://64.media.tumblr.com/085735941d6685e3e7b9173641226a5e/tumblr_p4vqvrVfca1x72yt1o3_500.png";  
-        imgIndex[2] = "https://64.media.tumblr.com/34f1d8b45adb53f92214e4d233c527a8/tumblr_p4vqvrVfca1x72yt1o4_500.jpg";  
-        imgIndex[3] = "https://i.pinimg.com/564x/e9/66/79/e96679e74561ad9c3b42738f71d44924.jpg";  
-
-        var result = '';
-        var numberOfImg = window.prompt("How many sakura images do you want?\nPrefer choose a number from 1 to 4");
-        var imgs = '';
-        for (var i =0; i<numberOfImg; i++ ){
-            if (i < 4){
-            imgs = '<img src="'+imgIndex[i]+'" width="250" height="300" />';}
-            result+= imgs;  
+       */
+        function generator() { 
+            var imgIndex = new Array(); 
+            //insert the URL of images in array  
+            imgIndex[0] = "https://64.media.tumblr.com/0a423f1560b6379d350f3c997e73f57e/tumblr_p4vqvrVfca1x72yt1o1_500.png";  
+            imgIndex[1] = "https://64.media.tumblr.com/085735941d6685e3e7b9173641226a5e/tumblr_p4vqvrVfca1x72yt1o3_500.png";  
+            imgIndex[2] = "https://64.media.tumblr.com/34f1d8b45adb53f92214e4d233c527a8/tumblr_p4vqvrVfca1x72yt1o4_500.jpg";  
+            imgIndex[3] = "https://i.pinimg.com/564x/e9/66/79/e96679e74561ad9c3b42738f71d44924.jpg";  
+    
+            var result = '';
+            var numberOfImg = window.prompt("How many sakura images do you want?\nPrefer choose a number from 1 to 4");
+            var imgs = '';
+            if (numberOfImg>=1 && numberOfImg < 5){ 
+            for (var i =0; i<numberOfImg; i++ ){
+                //if (i>=0 && i < 4){ 
+                imgs = '<img src="'+imgIndex[i]+'" width="250" height="300" style= "padding:6px 10px; margin-left:50px;"/>';
+                result+= imgs;   
+             }  
+            }
+                else{
+                alert("please a number between 1 to 4")
+                generator() =0;
+                }    
+           // }
+            document.write(result);
+            
         }
-        document.write(result);
+        
+        generator()
+
         var z = confirm("Welcome "+ x + ", Hope you liked it!");
         console.log("username prompt value = "+x);
         console.log("confirm value = "+z);
         //Background Color
 
         //Functions helps reuse code; Just while define it once, use it any time.
+       
+        
+
+        
+        
+        
+   
